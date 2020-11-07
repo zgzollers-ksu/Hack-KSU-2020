23f437d42f19c498468848bced471a781bb5055d
will1008m <41597375+will1008m@users.noreply.github.com>
2020-11-07 14:57:04 -0600

    Update spider.py

--- spider.py
+++ spider.py
@@ -1,10 +1,36 @@
-import requests as req
+import requests
 from bs4 import BeautifulSoup as bs
-
+from textblob import TextBlob as tb
+from nltk.corpus import wordnet
 
 class spider:
     def __init__(self):
         pass
 
     def make_request(self, url: str):
-        pass
+        page = requests.get(url)
+
+		soup = bs(page.content, 'html.parser')
+
+		blob = tb(str(soup.find_all("p")))
+
+		nouns = [] 
+		for i in blob.noun_phrases: 
+		    if i not in nouns and i.isalnum(): 
+		        nouns.append(i)
+
+		synonyms = []
+		for i in nouns:
+			for syn in wordnet.synsets(i):
+				for lm in syn.lemmas():
+					synonyms.append(lm.name())
+
+		nouns = nouns + synonyms
+		#print(nouns)
+
+		fixed_nouns = [] 
+		for i in nouns: 
+		    if i not in fixed_nouns: 
+		        fixed_nouns.append(i)
+
+		return fixed_nouns
