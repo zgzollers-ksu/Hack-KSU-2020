1e1f813ae0e1fbcd891b009424284b2f7e14a2c5
zgzollers-ksu <73486789+zgzollers-ksu@users.noreply.github.com>
2020-11-07 14:38:29 -0600

    Update main.py
    implement find_match() method

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-import spider
+import hashlib
+
+def find_match(options: dict, hash: list):
+    for item in hash:
+        if item in dict.keys():
+            return dict[item]
+
+    return "\0"
+
