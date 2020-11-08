6b9760e72f9624b4c46e9dee95d3721d5f0559ac
zgzollers-ksu <73486789+zgzollers-ksu@users.noreply.github.com>
2020-11-07 23:04:46 -0600

    Update passwords.py
    Implemented generate_passwords

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import hashlib
 import passwords
 
+
 def find_match(options: dict, hash: list):
     for item in hash:
         if item in dict.keys():
             return dict[item]
 
     return "\0"
+
+passwords.generate_passwords(1)
